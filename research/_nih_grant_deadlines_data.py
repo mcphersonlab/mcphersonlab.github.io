15bb98fbb6e600d57ea6b2eda481b615a4b69b43
Jacob K. McPherson <70453018+JacobKMcPherson@users.noreply.github.com>
2026-03-08 16:15:23 +0000

    added nih deadlines to calendar

---
+++ research/_nih_grant_deadlines_data.py
@@ -0,0 +1,56 @@
+nih_grant_deadlines = [
+    {
+        "label": "NIH R01/U01 New",
+        "month_days": ["02-05", "06-05", "10-05"],
+        "detail": "R01/U01 new applications",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH R01/U01 Renewal/Resubmission/Revision",
+        "month_days": ["03-05", "07-05", "11-05"],
+        "detail": "R01/U01 renewal/resubmission/revision",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH K Series New",
+        "month_days": ["02-12", "06-12", "10-12"],
+        "detail": "Career development awards",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH K Series Renewal/Resubmission/Revision",
+        "month_days": ["03-12", "07-12", "11-12"],
+        "detail": "Career development renewals",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH R03/R21/R33/R34 Family New",
+        "month_days": ["02-16", "06-16", "10-16"],
+        "detail": "Small grant family",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH R03/R21/R33/R34 Family Renewal/Resubmission/Revision",
+        "month_days": ["03-16", "07-16", "11-16"],
+        "detail": "Small grant family renewals",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH R15 AREA",
+        "month_days": ["02-25", "06-25", "10-25"],
+        "detail": "AREA grants",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH F Series",
+        "month_days": ["04-08", "08-08", "12-08"],
+        "detail": "Fellowships",
+        "note": "Standard NIH due date",
+    },
+    {
+        "label": "NIH R13/U13 Conference Grants",
+        "month_days": ["04-12", "08-12", "12-12"],
+        "detail": "Conference grants",
+        "note": "Standard NIH due date",
+    },
+]
